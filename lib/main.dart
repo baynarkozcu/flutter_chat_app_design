c7e6546ce2eee72c94b307caabc7b60241a21248
baynarkozcu <enesozudogruu@gmail.com>
2022-07-25 22:57:37 +0300

    firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:chat_app_design/core/extensions/theme_extensions.dart';
+import 'package:chat_app_design/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -7,7 +9,11 @@ import 'feature/home/view/home_view.dart';
 import 'feature/login/view/login_view.dart';
 import 'feature/master_page.dart';
 
-void main() {
+void main()  async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(create: (_) => ThemeManager.instance),
