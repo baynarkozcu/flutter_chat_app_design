de46799b064a482504f4ea01d13342abc94c8053
baynarkozcu <enesozudogruu@gmail.com>
2022-07-31 20:38:30 +0300

    user modal added

--- lib/feature/login/services/login_services.dart
+++ lib/feature/login/services/login_services.dart
@@ -11,21 +11,15 @@ class LoginService {
   LoginService._init();
 
   Future<UserCredential> signInWithGoogle() async {
-    // Trigger the authentication flow
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-    // Obtain the auth details from the request
     final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
 
-    // Create a new credential
     final credential = GoogleAuthProvider.credential(
       accessToken: googleAuth?.accessToken,
       idToken: googleAuth?.idToken,
     );
 
-    // Once signed in, return the UserCredential
     final result = await FirebaseAuth.instance.signInWithCredential(credential);
-    print("Result :" + result.user!.email!);
     return result;
   }
 }
